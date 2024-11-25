dc5ebb35f6a4c4024e21b75c86ed5169d84149ca
Joshua Williams <joshuaw@hpe.com>
2024-11-24 22:15:11 -0800

    One more experiment

--- vendor/github.com/Cray-HPE/hms-trs-app-api/v2/pkg/trs_http_api/trshttp_local.go
+++ vendor/github.com/Cray-HPE/hms-trs-app-api/v2/pkg/trs_http_api/trshttp_local.go
@@ -443,16 +443,19 @@ func createClient(task *HttpTask, tloc *TRSHTTPLocal, clientType string) (client
 	}
 
 	// Wrap base transport with retryablehttp
-	retryabletr := &trsRoundTripper{
-		transport:                             tr,
-		closeIdleConnectionsFn:                tr.CloseIdleConnections,
-		timeLastClosedOrReachedZeroCloseCount: time.Now(),
-	}
+// XXXX
+//	retryabletr := &trsRoundTripper{
+//		transport:                             tr,
+//		closeIdleConnectionsFn:                tr.CloseIdleConnections,
+//		timeLastClosedOrReachedZeroCloseCount: time.Now(),
+//	}
 
 	// Create the httpretryable client and start configuring it
 	client = retryablehttp.NewClient()
 
-	client.HTTPClient.Transport = retryabletr
+// XXXX
+//	client.HTTPClient.Transport = retryabletr
+client.HTTPClient.Transport = tr
 
 	// We could set a global http timeout for all users of the client but
 	// that's a bit inflexible.  Let's keep it at the default (unlimited)
@@ -463,7 +466,8 @@ func createClient(task *HttpTask, tloc *TRSHTTPLocal, clientType string) (client
 	//client.HTTPClient.Timeout   = task.Timeout * 9 / 10
 
 	// Wrap httpretryable's DefaultRetryPolicy() so we can override
-	client.CheckRetry = retryabletr.trsCheckRetry
+// XXXX
+//	client.CheckRetry = retryabletr.trsCheckRetry
 
 	// Configure the httpretryable client retry count
 	if (task.CPolicy.Retry.Retries >= 0) {
@@ -563,19 +567,23 @@ func ExecuteTask(tloc *TRSHTTPLocal, tct taskChannelTuple) {
 	// Wrap the request so trsCheckRetry() can keep its own retry count
 	// We'll attach this to the context further below so that it has access
 	// to it
-	trsWR := &trsWrappedReq{
-		orig:       tct.task.Request,        // Core request
-		retryCount: 0,                       // Counter for CLIC()
-		retryMax:   cpack.insecure.RetryMax, // CLIC() will need access to this
-		                                     // same for both secure & insecure
-	}
+// XXXX
+//	trsWR := &trsWrappedReq{
+//		orig:       tct.task.Request,        // Core request
+//		retryCount: 0,                       // Counter for CLIC()
+//		retryMax:   cpack.insecure.RetryMax, // CLIC() will need access to this
+//		                                     // same for both secure & insecure
+//	}
 
 	// Create child context with timeout and our own retry counter
 
 	baseCtx, cancel := context.WithTimeout(tloc.ctx, tct.task.Timeout)
-	ctxWithValue := context.WithValue(baseCtx, trsRetryCountKey, trsWR)
+// XXXX
+//	ctxWithValue := context.WithValue(baseCtx, trsRetryCountKey, trsWR)
 
-	tct.task.context = ctxWithValue
+// XXXX
+//	tct.task.context = ctxWithValue
+tct.task.context = baseCtx
 	tct.task.contextCancel = cancel
 
 	// Create a retryablehttp request using the caller's request
@@ -768,15 +776,6 @@ func (tloc *TRSHTTPLocal) Close(taskList *[]HttpTask) {
 		tloc.taskMutex.Unlock()
 
 	}
-// See if this helps memory leak
-for k := range tloc.clientMap {
-	if (tloc.clientMap[k].insecure != nil) {
-		tloc.clientMap[k].insecure.HTTPClient.CloseIdleConnections()
-	}
-	if (tloc.clientMap[k].secure != nil) {
-		tloc.clientMap[k].secure.HTTPClient.CloseIdleConnections()
-	}
-}
 
 	tloc.Logger.Tracef("Close() completed")
 }
